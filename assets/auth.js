3e3856d64059b06ee719049e143b7713dbb00db3
rasenin <romansenin15@gmail.com>
2019-04-30 19:20:51 -0700

    added login functionality

--- assets/auth.js
+++ assets/auth.js
@@ -1,35 +1,16 @@
 $(document).ready(function() {
-
-  // get data
-  db.collection('tasks').get().then(function(snapshot) {
-    console.log(snapshot.docs);
-  });
-
   // listen for auth status changes
   auth.onAuthStateChanged(function(user) {
     if (user) {
-      console.log("user logged in: ", user);
-    } else {
-      console.log("user logged out");
+      // get data
+      db.collection("users").doc(user.uid).collection("incomplete").onSnapshot(function(snapshot) {
+        setupTasks(snapshot.docs, "incomplete", user.uid);
+      });
+      db.collection("users").doc(user.uid).collection("complete").onSnapshot(function(snapshot) {
+        setupTasks(snapshot.docs, "complete", user.uid);
+      });
+      setupUI(user); // display greeting
     }
-  });
-
-
-  // signup
-  $(".form-signup").on("submit", function(event) {
-    event.preventDefault();
-
-    // get user info
-    const firstName = $("#firstName").val();
-    const lastName = $("#lastName").val();
-    const email = $("#emailInput").val();
-    const password = $("#passwordInput").val();
-
-    // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(function(cred) {
-      // document.getElementsByClassName('form-signup')[0].reset();
-      window.location = "notify.html"; // After successful sign up, user will be redirected to notify.html
-    });
   });
 
   // logout
@@ -37,19 +18,6 @@ $(document).ready(function() {
     event.preventDefault();
     auth.signOut().then(function() {
       window.location = "index.html"; // Go back to home page after sign out
-    });
-  });
-
-  // login
-  $(".form-signin").on("submit", function(event) {
-    event.preventDefault();
-
-    // get user info
-    const email = $("#loginEmail").val();
-    const password = $("#loginPassword").val();
-
-    auth.signInWithEmailAndPassword(email, password).then(function(cred) {
-      window.location = "notify.html";
     });
   });
 });
---
+++ assets/data.js
@@ -0,0 +1,147 @@
+let numIncomplete, numComplete;
+
+function setupUI(user) {
+  $(".card-title").text("Hi " + user.displayName+ "!");
+}
+
+let cred;
+
+// setup tasks
+const setupTasks = (data, collection, uid) => {
+  cred = uid;
+  if (collection === "incomplete") {
+    numIncomplete = data.length;
+
+    $("#incomplete-tasks").empty();
+
+    $.each(data, function(index, doc) {
+      const task = doc.data();
+      const li = $("<li>").append(`
+        <input type="checkbox" style="margin-bottom: 5px;" class="check-incomplete" id="check-incomplete-task-${index +
+          1}">
+        <div id=incomplete-task-${index +
+          1} class="task" style="margin-top: 5px;" value=${doc.id}>${
+        task.title
+      }</div>
+        <button class="delete-incomplete" id="delete-incomplete-task-${index +
+          1}">Delete</button>
+    `);
+      $("#incomplete-tasks").append(li);
+    });
+  } else {
+    numComplete = data.length;
+
+    $("#completed-tasks").empty();
+
+    $.each(data, function(index, doc) {
+      const task = doc.data();
+      const li = $("<li>").append(`
+      <input type="checkbox" style="margin-bottom: 5px;" class="check-complete" id="check-complete-task-${index +
+        1}">
+        <div id=complete-task-${index +
+          1} class="task" style="margin-top: 5px;" value=${doc.id}>${
+        task.title
+      }</div>
+        <button class="delete-complete" id="delete-complete-task-${index +
+          1}">Delete</button>
+    `);
+      $("#completed-tasks").append(li);
+    });
+    $('.check-complete').prop('checked', true);
+  }
+};
+
+// create new task
+$("#add-task-form").on("submit", function(event) {
+  event.preventDefault();
+
+  const newTask = $("#new-task")
+    .val()
+    .trim();
+
+  $("#new-task").val("");
+
+  // store new task in firebase
+  db.collection("users").doc(cred).collection("incomplete")
+    .add({
+      title: newTask
+    })
+    .then(() => {})
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
+$(document).on("click", ".delete-incomplete", function(event) {
+  event.preventDefault();
+
+  // get the id attribute of the delete
+  const id = $(this)
+    .attr("id")
+    .charAt($(this).attr("id").length - 1);
+
+  deleteTask("incomplete", id);
+});
+
+$(document).on("click", ".delete-complete", function(event) {
+  event.preventDefault();
+
+  // get the id attribute of the delete
+  const id = $(this)
+    .attr("id")
+    .charAt($(this).attr("id").length - 1);
+
+  deleteTask("complete", id);
+});
+
+$(document).on("click", ".check-incomplete", function(event) {
+
+  // get the id attribute of the check input
+  const id = $(this)
+    .attr("id")
+    .charAt($(this).attr("id").length - 1);
+
+  // store checked task in completed collection
+  db.collection("users").doc(cred).collection("complete")
+    .add({
+      title: $(`#incomplete-task-${id}`).text()
+    })
+    .then(() => {})
+    .catch(err => {
+      console.log(err.message);
+    });
+
+  // remove checked task from incomplete collection
+  deleteTask("incomplete", id);
+});
+
+$(document).on("click", ".check-complete", function(event) {
+
+  // get the id attribute of the check input
+  const id = $(this)
+    .attr("id")
+    .charAt($(this).attr("id").length - 1);
+
+  // store checked task in incomplete collection
+  db.collection("users").doc(cred).collection("incomplete")
+    .add({
+      title: $(`#complete-task-${id}`).text()
+    })
+    .then(() => {})
+    .catch(err => {
+      console.log(err.message);
+    });
+
+  // remove checked task from incomplete collection
+  deleteTask("complete", id);
+});
+
+function deleteTask(collectionName, id) {
+  db.collection("users").doc(cred).collection(collectionName)
+    .doc($(`#${collectionName}-task-${id}`).attr("value"))
+    .delete()
+    .then(function() {})
+    .catch(function(error) {
+      console.error("Error removing document: ", error);
+    });
+}
