ccce0866d93c1609cb4aa1dc4a14fc050829d9e5
paigesmith3794 <paigesmith3794@gmail.com>
2019-05-03 14:56:57 -0700

    included email validation API to the signup-login.js file

--- assets/signup-login.js
+++ assets/signup-login.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // signup
-  $(".form-signup").on("submit", function(event) {
+  $(".form-signup").on("submit", function (event) {
     event.preventDefault();
 
     // get user info
@@ -28,84 +28,77 @@ $(document).ready(function() {
       modal.style.display = "block";
 
       // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
+      span.onclick = function () {
         modal.style.display = "none";
       };
 
       // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
+      window.onclick = function (event) {
         if (event.target == modal) {
           modal.style.display = "none";
         }
       };
-    } else {
-      // sign up the user
-      auth.createUserWithEmailAndPassword(email, password).then(
-        function(cred) {
-          cred.user
-            .updateProfile({
-              displayName: `${firstName}`
-            })
-            .then(
-              function() {
-                window.location = "notify.html"; // After successful sign up, user will be redirected to notify.html
-              },
-              function(error) {
-                console.error(error);
-              }
-            );
-        },
-        function(error) {
-          const modalMessage = $("#modal-message");
-
-          modalMessage.text(error.message);
-
-          // Get the modal
-          var modal = document.getElementById("myModal");
-
-          // Get the <span> element that closes the modal
-          var span = document.getElementsByClassName("close")[0];
-
-          // open the modal
-          modal.style.display = "block";
-
-          // When the user clicks on <span> (x), close the modal
-          span.onclick = function() {
-            modal.style.display = "none";
-          };
-
-          // When the user clicks anywhere outside of the modal, close it
-          window.onclick = function(event) {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          };
-        }
-      );
     }
-  });
 
-  // login
-  $(".form-signin").on("submit", function(event) {
-    event.preventDefault();
+    // API Call to the email vaification API
+    var apiKey = "cc7a615c717948168775c14fe5dd4f30";
+    var queryURL = "https://api.zerobounce.net/v1/validatewithip?apikey=" + apiKey + "&email=" + email + "&ipAddress=156.124.12.145";
 
-    // get user info
-    const email = $("#loginEmail").val();
-    const password = $("#loginPassword").val();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response.status);
 
-    auth
-      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-      .then(function() {
-        // Existing and future Auth states are now persisted in the current
-        // session only. Closing the window would clear any existing state even
-        // if a user forgets to sign out.
-        // ...
-        // New sign-in will be persisted with session persistence.
-        return auth.signInWithEmailAndPassword(email, password).then(
-          function(cred) {
-            window.location = "notify.html";
+
+      var notValid = (response.status != "Valid");
+
+      if (notValid) {
+
+        const modalMessage = $("#modal-message");
+
+        modalMessage.text("This email is not valid.");
+
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // open the modal
+        modal.style.display = "block";
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+          modal.style.display = "none";
+        };
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+          // };
+        }
+      } else {
+
+        // sign up the user
+        auth.createUserWithEmailAndPassword(email, password).then(
+          function (cred) {
+            cred.user
+              .updateProfile({
+                displayName: `${firstName}`
+              })
+              .then(
+                function () {
+                  window.location = "notify.html"; // After successful sign up, user will be redirected to notify.html
+                },
+                function (error) {
+                  console.error(error);
+                }
+              );
           },
-          function(error) {
+          function (error) {
             const modalMessage = $("#modal-message");
 
             modalMessage.text(error.message);
@@ -120,12 +113,64 @@ $(document).ready(function() {
             modal.style.display = "block";
 
             // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
+            span.onclick = function () {
               modal.style.display = "none";
             };
 
             // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
+            window.onclick = function (event) {
+              if (event.target == modal) {
+                modal.style.display = "none";
+              }
+            };
+          });
+      }
+
+    });
+  });
+
+
+  // login
+  $(".form-signin").on("submit", function (event) {
+    event.preventDefault();
+
+    // get user info
+    const email = $("#loginEmail").val();
+    const password = $("#loginPassword").val();
+
+    auth
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(function () {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return auth.signInWithEmailAndPassword(email, password).then(
+          function (cred) {
+            window.location = "notify.html";
+          },
+          function (error) {
+            const modalMessage = $("#modal-message");
+
+            modalMessage.text(error.message);
+
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // open the modal
+            modal.style.display = "block";
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+              modal.style.display = "none";
+            };
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function (event) {
               if (event.target == modal) {
                 modal.style.display = "none";
               }
@@ -133,7 +178,7 @@ $(document).ready(function() {
           }
         );
       })
-      .catch(function(error) {
+      .catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -172,7 +217,8 @@ $(document).ready(function() {
     // );
   });
 
-  $("#signup-button").on("click", function() {
+  $("#signup-button").on("click", function () {
     window.location = "sign-up.html";
   });
+
 });
